205d6339c81cd54c16681e8a5a56b3ca8dd77956
Jose Bautista <jcbautista95@gmail.com>
2021-02-22 20:36:46 -0600

    kill process

--- src/App.js
+++ src/App.js
@@ -3,8 +3,30 @@ import './App.css';
 import Cpu from "./components/cpu";
 import Graph from "./components/graph";
 import TreeView from "./components/treeView";
+import axios from 'axios';
+import React, { useState} from 'react';
+
 
 function App() {
+  const [process, setProcess] = useState('')
+
+  async function  handleClick(e) {
+    console.log(process);
+    e.preventDefault();
+    console.log(process.value);
+    let obj = {
+      "id":process.value,
+      "mensaje":"main"
+    }
+    ;
+    console.log(obj);
+    let response = await axios.post('http://3.141.196.231:3000/delProc',obj);
+    console.log(response.data);
+
+  }
+  function handleChange(event) {
+    setProcess({value: event.target.value});
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,10 +39,15 @@ function App() {
         <br/>
         <div className="App-treeView" >
           <TreeView/>
+          <br/>
+          <input type="text"  onChange={handleChange}></input>
+          <button onClick={handleClick}>Kill Proccess</button>
         </div>
+        
       </header>
       
     </div>
+
   );
 }
 
